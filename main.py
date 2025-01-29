d80d51f3420b5cf22ac30c23fa31d5a22ea24e0f
Frerijs <edijs.freijs@gmail.com>
2025-01-29 12:49:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import streamlit as st
 import requests
 
-headers = {
-    "User-Agent": "MyStreamlitApp/1.0 (myemail@domain.com)"
-}
-params = {
-    "format": "json",
-    "limit": 5,
-    "q": "Talsi"
-}
-r = requests.get("https://nominatim.openstreetmap.org/search", params=params, headers=headers)
-print("Status code:", r.status_code)
-print("Response:", r.text)
+def nominatim_search(q):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {"format":"json", "limit":5, "q": q}
+    headers = {"User-Agent": "MyStreamlitApp/1.0 (myemail@domain.com)"}
+    r = requests.get(url, params=params, headers=headers)
+    st.write("DEBUG status code:", r.status_code)
+    st.write("DEBUG text:", r.text)
+    r.raise_for_status()
+    return r.json()
+
+address = st.text_input("Ievadiet vietu:")
+if st.button("Meklēt"):
+    results = nominatim_search(address.strip())
+    st.write(results)
