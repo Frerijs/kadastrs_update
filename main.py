7deec8c60b78d0d98cdaef6700ea35012a3c7208
Frerijs <edijs.freijs@gmail.com>
2025-02-06 09:52:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,9 +43,6 @@ translations = {
         "methods": [
             'Augšupielādējiet iepriekš sagatavotu noslēgtas kontūras failu .DXF vai .SHP formātā',
             'Zīmējiet uz kartes noslēgtu kontūru',
-        ],
-        "radio_label": "Izvēlieties veidu, kā iegūt datus:",
-         "methods": [
             'Ievadiet vienu vai vairākus kadastra numurus un iegūstiet datus',
             'Ievadiet vienu vai vairākus kadastra numurus un iegūstiet datus gan par filtrētajiem kadastra numuriem, gan par pieskarošiem'
         ],
