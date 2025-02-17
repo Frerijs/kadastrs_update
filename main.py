fea170d2ed2be925088053cf299b95b01d26340c
Frerijs <edijs.freijs@gmail.com>
2025-02-17 09:48:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,24 +38,13 @@ APP_TYPE = "web"
 
 # Tulkošanas vārdnīca
 translations = {
-    "Latviešu": {
-        # Pirmais radio izvēles bloks
-method = st.radio(
-    "Izvēlieties veidu, kā iegūt datus:",
-    [
-        "Augšupielādējiet iepriekš sagatavotu noslēgtas kontūras failu .DXF vai .SHP formātā",
-        "Zīmējiet uz kartes noslēgtu kontūru"
-    ]
-)
-
-# Otrais radio izvēles bloks ar jaunu labeli
-cadaster_method = st.radio(
-    "Meklēt pēc kadastra apzīmējuma un iegūt datus:",
-    [
-        "Tikai ievadītajiem kadastra apzīmējumiem",
-        "Ievadītajiem kadastra apzīmējumiem un pierobežniekiem"
-    ]
-)
+ "radio_label": "Izvēlieties veidu, kā iegūt datus:",
+        "methods": [
+            'Augšupielādējiet iepriekš sagatavotu noslēgtas kontūras failu .DXF vai .SHP formātā',
+            'Zīmējiet uz kartes noslēgtu kontūru',
+            'Tikai ievadītajiem kadastra apzīmējumiem',
+            'Ievadītajiem kadastra apzīmējumiem un pierobežniekiem'
+        ],
         "title": "Kadastra apzīmējumu saraksta lejuplāde (ZV robežas un apzīmējumi)",
         "language_label": "Valoda / Language",
         "upload_instruction": "Augšupielādējiet slēgtu kontūru vai vairākas kontūras vienā no atbalstītajiem failu formātiem:",
